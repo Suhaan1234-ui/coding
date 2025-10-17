69b0fafaa3755c4715f534cbcc1d85059ede0502
suhaan1234-ui <vijaysuhaan@gmail.com>
2025-10-17 19:34:55 +0530

    problems

--- python/problem10.py
+++ python/problem10.py
@@ -2,5 +2,7 @@ letter = '''Dear <|Name|>,
 You are selected! 
 <|Date|> '''
 
-print(letter.replace("<|Name|>", "Harry").replace("<|Date|>", "24 September 2050"))
+print(letter.replace("<|Name|>", "Harry").replace("<|Date|>", "24 September 2050"))#THIS IA A NEW STRING NOT THE
+                                                                                   # ORIGINAL ONE
 # this is called method chaining
+print(letter)  # original letter remains unchanged ie strings are immutable
---
+++ python/problem11.py
@@ -0,0 +1,4 @@
+a="I am learning  Python"#variable are initalized with lower case letter, upper case letter for classes
+print(a.find("  "))
+# find gives the index of substring asked in the given string
+# if not found it gives -1
